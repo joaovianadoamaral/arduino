6d605d4295c3d999623c030d08f5fd89c6a6ce9b
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-06-25 15:25:51 -0300

    fiz o LED variar a luminosidade através de portas PWM

--- programas/aula05/aula05.ino
+++ programas/aula05/aula05.ino
@@ -1,9 +1,37 @@
+#define led 3
+#define maximo 255
+#define minimo 0
+#define  tmp 100
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(led,OUTPUT);
 }
+
+void acende(void);
+void apaga(void);
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //digitgalWrite(led,HIGH); apenas verdadeiro ou falso
 
+  //vamos criar um programa que o led acenda progressivamente e apague progressivamente
+  acende();
+  apaga();
+  
+}
+void acende(void){
+  //acende, sem delay faz mt rapido
+  for(int i=minimo;i<maximo;i++){
+    analogWrite(led,i);//portas analogicas ou pwm
+    delay(tmp);
+  }
+}
+void apaga(void){
+    //apaga, sem delay apaga mt rapido.
+  for(int i=maximo;i>minimo;i--){
+    analogWrite(led,i);//portas analogicas ou pwm
+    delay(tmp);
+  }
 }
