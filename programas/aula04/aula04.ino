6e2841313ad476dfe145a43e3c97074113c46aa4
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-09-07 16:49:39 -0300

    semaforo + trabalho domotica

--- programas/aula04/aula04.ino
+++ programas/aula04/aula04.ino
@@ -3,9 +3,9 @@
 #define vermelho 8
 
 //deu erro pq coloquei o mesmo nome nos define,cuidado 
-void f_vermelho(int tmp);
-void f_amarelo(int tmp);
-void f_verde(int tmp);
+void f_vermelho(float tmp);
+void f_amarelo(float tmp);
+void f_verde(float tmp);
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,23 +16,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-    f_vermelho(7);
-    f_verde(9);
-    f_amarelo(2);
+    f_vermelho(4);
+    f_verde(4);
+    f_amarelo(4);
 }
-void f_vermelho(int tmp){
+void f_vermelho(float tmp){
   digitalWrite(vermelho,HIGH);
   digitalWrite(amarelo,LOW);
   digitalWrite(verde,LOW);
   delay(tmp*1000);
 }
-void f_amarelo(int tmp){
+void f_amarelo(float tmp){
   digitalWrite(vermelho,LOW);
   digitalWrite(amarelo,HIGH);
   digitalWrite(verde,LOW);
   delay(tmp*1000);
 }
-void f_verde(int tmp){
+void f_verde(float tmp){
   digitalWrite(vermelho,LOW);
   digitalWrite(amarelo,LOW);
   digitalWrite(verde,HIGH);
--- programas/aula07/aula07.ino
+++ programas/aula07/aula07.ino
@@ -1,9 +1,28 @@
+#define D0 16
+#define D1 5 // I2C Bus SCL (clock)
+#define D2 4 // I2C Bus SDA (dados)
+#define D3 0
+#define D4 2 // "LED_BUILTIN", com lógica invertida
+#define D5 14 // SPI Bus SCK (clock)
+#define D6 12 // SPI Bus MISO
+#define D7 13 // SPI Bus MOSI
+#define D8 15 // SPI Bus SS (CS)
+#define D9 3 // RX0 (Serial console)
+#define D10 1 // TX0 (Serial console)
+
+#define som 3
+#define led 4
+#define pino_tasmota 5
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(D5,INPUT);
+  pinMode(D6,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(digitalRead(D5)==HIGH)
+    digitalWrite(D6,HIGH);
+   else if(digitalRead(D5)==LOW)
+    digitalWrite(D6,LOW);
 }
